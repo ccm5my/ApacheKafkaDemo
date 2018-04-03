f581d33803ec3b2bfbfb16cc77bb46e06d205ca9
cmoore <cmooore@captechventures.com>
2018-04-03 11:22:00 -0400

    Creating a demo for Apache Kafka

---
+++ src/Cam/SimpleProducer.java
@@ -0,0 +1,58 @@
+package Cam;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.*;
+
+public class SimpleProducer {
+	public static void main(String[] args) throws Exception {
+		if(args.length==0) {
+			System.out.println("Enter topic name");
+		}
+	//Assign topicName to string variable.
+	String topicName = "SimpleProducer2";
+	
+	//create instance for properties to access producer configs.
+	Properties props = new Properties();
+	
+	//assign localhostId;
+	props.put("bootstrap.servers", "localhost:9092");
+	
+	//Set acknowledgements for producer requests
+	props.put("acks", "all");
+	
+	//If the request fails, the producer can automatically retry.
+	props.put("retries", 0);
+	
+	//Specify buffer size in config
+	props.put("batch.size", 16384);
+	
+	//Reduce the no of requests less than 0
+	props.put("linger.ms", 1);
+	
+	//the buffer.memory controls the total memory available to the producer
+	props.put("buffer.memory", 33554432);
+	
+	props.put("key.serializer", 
+	         "org.apache.kafka.common.serialization.StringSerializer");
+	         
+	props.put("value.serializer", 
+	         "org.apache.kafka.common.serialization.StringSerializer");
+	
+	Producer<String, String> producer = new KafkaProducer<>(props);
+	
+	for(int i = 0; i<10; i++)  {
+		producer.send(new ProducerRecord<String,String>(topicName, Integer.toString(i), Integer.toString(i)));
+		System.out.println(i);
+	}
+		
+		System.out.println("Message sent successfully");
+	
+		producer.close();
+		
+		
+	}
+}
+	
+
+
